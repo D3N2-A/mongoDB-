84172b2731faa8899c5249de333421a4b63c026f
D3N2-A <anmolsharma999937@gmail.com>
2022-06-21 09:33:46 +0530

    db

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/carsDB");
+
+const carSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  rating: Number,
+  horsepower: Number,
+});
+
+const Car = mongoose.model("car", carSchema);
+
+const GT = new Car({
+  _id: 1,
+  name: "GT",
+  rating: 8.5,
+  horsepower: 1200,
+});
+
+const peopleSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  age: Number,
+});
+
+const People = mongoose.model("people", peopleSchema);
+
+const Nigga = new People({
+  _id: 1,
+  name: "Nigga Singh",
+  age: 19,
+});
+
+Nigga.save();
